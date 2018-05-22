54af9419917ef3c2bddb41d02e122fe2d3addaab
FObermaier <felix.obermaier@netcologne.de>
2018-05-22 13:41:57 +0200

    Update to GeoAPI.Core usage Update nuspec

--- NetTopologySuite.Features.Test/FeaturesTestSetup.cs
+++ NetTopologySuite.Features.Test/FeaturesTestSetup.cs
@@ -2,13 +2,19 @@
 
 namespace NetTopologySuite.Test
 {
+    /// <summary>
+    /// Utility class to make sure that NTS is set up and registered properly
+    /// </summary>
     [NUnit.Framework.SetUpFixture]
     public class FeaturesTestSetup
     {
+        /// <summary>
+        /// Method to wire NTS
+        /// </summary>
         [NUnit.Framework.OneTimeSetUp]
         public void OneTimeSetUp()
         {
-            NetTopologySuiteBootstrapper.Bootstrap();
+            GeometryServiceProvider.SetInstanceIfNotAlreadySetDirectly(NtsGeometryServices.Instance);
         }
     }
 }
