7298031d09a3b6b7a672c0a6c2af838c69feadad
Joe Amenta <airbreather@linux.com>
2019-08-23 05:50:44 -0400

    Add some tests specifically for AttributesTable

--- test/NetTopologySuite.Features.Test/FeatureCollectionTest.cs
+++ test/NetTopologySuite.Features.Test/FeatureCollectionTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 using NetTopologySuite.Geometries;
--- test/NetTopologySuite.Features.Test/FeatureTest.cs
+++ test/NetTopologySuite.Features.Test/FeatureTest.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-using NetTopologySuite.Geometries;
+﻿using NetTopologySuite.Geometries;
 
 using NUnit.Framework;
 
