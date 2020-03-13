85f9ae06da180298cc569ee8876e2f2390bf1f02
Joe Amenta <airbreather@linux.com>
2020-03-13 09:17:07 -0400

    Support separate "id" values on features. (#11)

---
+++ src/NetTopologySuite.Features/IUnique.cs
@@ -0,0 +1,13 @@
+﻿namespace NetTopologySuite.Features
+{
+    /// <summary>
+    /// Interface for things tagged by an identifier that's assumed to be unique.
+    /// </summary>
+    public interface IUnique
+    {
+        /// <summary>
+        /// Gets the identifier of this object (assumed unique).
+        /// </summary>
+        object Id { get; }
+    }
+}
